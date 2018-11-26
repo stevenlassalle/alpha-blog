b519692b50a8b60a3a97017e9aa891dadcbefd5f
Steven Lassalle <stevenlassalle7@gmail.com>
2018-11-26 03:22:10 +0000

    Create Rails application and set up home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+    
+   def home
+       
+   end
+   
+   def about
+       
+   end
+   
+   def blog
+       
+   end
+   
+   def contactUs
+       
+   end
+    
+end
